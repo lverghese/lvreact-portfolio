603b4d80297d7117758778d6a6954554378dfb74
lverghese <loganverghese@gmail.com>
2021-10-29 20:10:28 -0400

    JS script for dropdown and nav styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,8 +4,9 @@ import pic from '../../images/Header.JPG'
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
-  <a className="navbar-brand text-light" href="/"><img className='logo' src={pic} alt='Profile'/></a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+
+  <a className="navbar-brand text-light" href="/"><img className='logo float-left' src={pic} alt='Profile'/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -16,10 +17,20 @@ const Nav = () => {
         <a className="nav-link" href="/">Home <span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">Link</a>
+        <a className="nav-link" href="/">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Resume</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Contact</a>
       </li>
     </ul>
   </div>
+
 </nav>
     )
 }
