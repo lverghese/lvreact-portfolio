0fd204795e1ebaa9a45360cbc98541825333b160
lverghese <loganverghese@gmail.com>
2021-10-29 22:26:55 -0400

    link styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from './components/Nav'
+import Header from './components/Header';
 
 function App() {
   return (
-    <Nav></Nav>
+    <>
+    <Nav />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>My React Portfolio</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
