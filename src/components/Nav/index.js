b97c2947db28fe7c8054f53ae21b6ee7b0938878
lverghese <loganverghese@gmail.com>
2021-10-29 20:25:52 -0400

    navbar icon

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import pic from '../../images/Header.JPG'
+// react font awesome 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
     return (
@@ -8,7 +11,7 @@ const Nav = () => {
 
   <a className="navbar-brand text-light" href="/"><img className='logo float-left' src={pic} alt='Profile'/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
