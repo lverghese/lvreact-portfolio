fa4b9f9211cb96221d91607256b4ab80a54ae2ac
lverghese <loganverghese@gmail.com>
2021-11-04 15:05:57 -0400

    particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from 'react-particles-js';
 import Nav from './components/Nav'
 import Header from './components/Header';
 
 function App() {
   return (
     <>
+    <Particles 
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#817e7e"
+            }
+          }
+        }
+      }}
+    
+    />
     <Nav />
     <Header />
     </>
